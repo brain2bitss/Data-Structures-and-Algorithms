67d469c99d54678588b04581af39d1d82c0ac345
brain2bitss <brain2bitss@gmail.com>
2026-05-14 13:10:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (49.81%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int nodes = 2; nodes <= n; nodes++){
+            int total = 0;
+            for(int root = 1; root <= nodes; root++){
+                int left = root - 1;
+                int right = nodes - root;
+
+                total = total + dp[left] * dp[right];
+            }
+            dp[nodes] = total;
+        }
+        return dp[n];
+    }
+}
