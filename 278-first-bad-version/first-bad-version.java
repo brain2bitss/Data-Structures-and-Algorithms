17a60f65a87d600f775b869dc9f563feb0132951
brain2bitss <brain2bitss@gmail.com>
2026-06-05 22:25:14 +0530

    Time: 13 ms (99.84%) | Memory: 42.3 MB (16.19%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -4,10 +4,10 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1; int right = n; int mid = -1;
-        while(left <= right){
+        while(left < right){
             mid = left + (right - left)/2;
             if(isBadVersion(mid)){
-                right = mid - 1;
+                right = mid;
             }else{
                 left = mid + 1;
             }
