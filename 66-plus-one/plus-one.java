a707b4b478a7b0669c63c337c4b35659d4b1faa2
brain2bitss <brain2bitss@gmail.com>
2026-06-19 19:55:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (42.12%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length - 1;
+
+        for(int i = length; i >= 0; i--){
+            if(digits[i] < 9){
+                ++digits[i];
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
