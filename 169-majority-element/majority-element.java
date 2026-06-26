3e064a6be5bfdd8f3d9fa3f909524494728011cf
brain2bitss <brain2bitss@gmail.com>
2026-06-26 09:44:49 +0530

    Time: 18 ms (12.76%) | Memory: 52.3 MB (96.96%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,35 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
-
-        // Boyer-Moore Voting Algorithm
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int val = map.get(nums[i]) + 1;
+                map.put(nums[i], val);
+            }else{
+                map.put(nums[i], 1);
             }
-            count += (num == candidate) ? 1 : -1;
-        }
 
-        return candidate;
+            if(map.get(nums[i]) > nums.length / 2){
+                return nums[i];
+            }
+        }
+        return -1;
     }
 }
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int count = 0;
+//         int candidate = 0;
+
+//         // Boyer-Moore Voting Algorithm
+         
+//         for (int num : nums) {
+//             if (count == 0) {
+//                 candidate = num;
+//             }
+//             count += (num == candidate) ? 1 : -1;
+//         }
+//         return candidate;
+//     }
+// }
