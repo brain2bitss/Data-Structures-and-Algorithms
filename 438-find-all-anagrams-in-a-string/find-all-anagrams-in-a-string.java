010b175a245f993f83aab53c9c3897c3b332dcf7
brain2bitss <brain2bitss@gmail.com>
2026-06-28 19:12:58 +0530

    Time: 13 ms (44.13%) | Memory: 46.7 MB (82.45%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -3,26 +3,21 @@ class Solution {
         List<Integer> result = new ArrayList<>();
         if (s.length() < p.length()) return result;
         
-        // Frequency arrays for characters 'a' to 'z'
         int[] pCount = new int[26];
         int[] sCount = new int[26];
         
-        // Initialize frequency count for pattern p
         for (char c : p.toCharArray()) {
             pCount[c - 'a']++;
         }
         
-        // Sliding window over s
         for (int i = 0; i < s.length(); i++) {
-            // Add current character to window
+            
             sCount[s.charAt(i) - 'a']++;
             
-            // Remove character that's no longer in window
             if (i >= p.length()) {
                 sCount[s.charAt(i - p.length()) - 'a']--;
             }
             
-            // Compare frequency arrays
             if (Arrays.equals(sCount, pCount)) {
                 result.add(i - p.length() + 1);
             }
