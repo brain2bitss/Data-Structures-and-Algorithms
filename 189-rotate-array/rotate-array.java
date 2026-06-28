ec627dda29b9ae23f4ef9935a7534f3cb4073a0c
brain2bitss <brain2bitss@gmail.com>
2026-06-28 14:53:48 +0530

    Time: 2 ms (20.46%) | Memory: 61.3 MB (54.22%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,43 +1,23 @@
 class Solution {
-    //CODED BY ADHI
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-
-        //BASE CASE
-        if(n == 1 || n == 0){
-            return;
-        }
-
-        int F = 0; int E = n - 1;
-
-        //BASE CASE
-         k = k % n;
         if(k == 0) return;
 
-        //REVERSE THE ENTIRE ARRAY 
-        function(nums, n, F, E);
+        k = k % nums.length;
 
-        
-
-        //REVERSE THE FIRST PART OF THE 
-        F = 0; E = k - 1;
-        function(nums, k, F, E);
-
-        //REVERSE REMAINING PART OF THE ARRAY
-        F = k; E = n - 1;
-        int rem_length = n - k;
-        function(nums, rem_length, F, E);
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
-    private void function(int[] nums, int n, int F, int E){
-        for(int i = 0; i < n; i++){
-            int temp = nums[F];
-            nums[F] = nums[E];
-            nums[E] = temp; 
-            F++;
-            E--;
-            if(F == E ||  F > E){
-                break;
-            }
+
+    private void reverse(int[] nums, int start, int end){
+        int i = start;
+        int j = end;
+        while(i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
     }
 }
