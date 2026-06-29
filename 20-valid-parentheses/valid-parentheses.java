537cb3745ad0ba4b9993a9264facf5b509c63258
brain2bitss <brain2bitss@gmail.com>
2026-06-29 09:25:02 +0530

    Time: 7 ms (5.82%) | Memory: 43.8 MB (5.95%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        if (s.length() == 0) {
+            return true;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            if(!stack.isEmpty() && s.charAt(i) == ')' && stack.peek() == '('){
+                stack.pop();
+            }else if(!stack.isEmpty() && s.charAt(i) == '}' && stack.peek() == '{'){
+                stack.pop();
+            }else if(!stack.isEmpty() && s.charAt(i) == ']' && stack.peek() == '['){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.isEmpty();
+    }
+}
