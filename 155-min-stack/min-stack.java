7a86a7ece0f4b0d42fc1a69158463560c9f0a917
brain2bitss <brain2bitss@gmail.com>
2026-06-29 11:24:32 +0530

    Time: 37 ms (8.23%) | Memory: 99.9 MB (25.56%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,40 +1,31 @@
-class MinStack {
-    private Stack<Integer> stack = new Stack<>();
-    private Stack<Integer> minStack = new Stack<>();
-    public MinStack() {
-        
-    }
-    
-    public void push(int value) {
-        if(!minStack.isEmpty()){
-            int min = Math.min(minStack.peek(),value);
-            minStack.push(min);
-            stack.push(value);
-        }else{
-            stack.push(value);
-            minStack.push(value);
-        } 
-    }
-    
-    public void pop() {
-        stack.pop();
-        minStack.pop();
-    }
-    
-    public int top() {
-        return stack.peek();
-    }
-    
-    public int getMin() {
-        return minStack.peek();
-    }
-}
+import java.util.Stack;
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(value);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+class MinStack { 
+    private Stack<Integer> stack = new Stack<>(); 
+    private Stack<Integer> minStack = new Stack<>(); 
+
+    public MinStack() { 
+    } 
+    
+    public void push(int value) { 
+        stack.push(value); 
+        if (minStack.isEmpty()) {
+            minStack.push(value);
+        } else {
+            minStack.push(Math.min(value, minStack.peek()));
+        }
+    } 
+
+    public void pop() { 
+        stack.pop(); 
+        minStack.pop(); 
+    } 
+
+    public int top() { 
+        return stack.peek(); 
+    } 
+
+    public int getMin() { 
+        return minStack.peek(); 
+    } 
+}
