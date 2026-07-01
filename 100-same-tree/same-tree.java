226dd0caf300a8cf05f1d0cef7c55b339021e916
brain2bitss <brain2bitss@gmail.com>
2026-07-01 14:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (51.72%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -21,10 +21,10 @@ class Solution {
         if (p == null || q == null) {
             return false;
         }
-        if (p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        } else {
+        if (p.val != q.val) {
             return false;
         }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        
     }
 }
