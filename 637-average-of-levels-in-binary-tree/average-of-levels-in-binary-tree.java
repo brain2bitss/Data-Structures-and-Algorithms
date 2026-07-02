b6223fec1e8b9ab9e93839f85bea8446c09f50b2
brain2bitss <brain2bitss@gmail.com>
2026-07-02 12:20:26 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (26.63%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -1,25 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> result = new ArrayList<>();
-        if (root == null) return result;
-        
-        Queue<TreeNode> queue = new ArrayDeque<>(); 
-        queue.offer(root);
-        
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            long sum = 0; 
-            
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.poll();
-                sum += node.val;
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-            
-            result.add((double) sum / levelSize);
+    public void function(TreeNode root, int level, ArrayList<Integer> count, ArrayList<Long> ans){
+        if(root == null){
+            return;
         }
+        if(ans.size() == level){
+            count.add(1);
+            ans.add((long)root.val);
+        }
+        else{
+            count.set(level, count.get(level) + 1);
+            ans.set(level, ans.get(level) + root.val);
+        }
+        function(root.left, level + 1, count, ans);
+        function(root.right, level + 1, count, ans);
+    }
+    public List<Double> averageOfLevels(TreeNode root) {
         
+        ArrayList<Integer> count = new ArrayList<>();
+        ArrayList<Long> ans = new ArrayList<>();
+        function(root, 0, count, ans);
+
+        List<Double> result = new ArrayList<>();
+        for(int i = 0; i < ans.size(); i++){
+            result.add((double)ans.get(i) / count.get(i));
+        }
         return result;
     }
 }
+
+
+// class Solution {
+//     public List<Double> averageOfLevels(TreeNode root) {
+//         List<Double> result = new ArrayList<>();
+//         if (root == null) return result;
+        
+//         Queue<TreeNode> queue = new LinkedList<>(); 
+//         queue.offer(root);
+        
+//         while (!queue.isEmpty()) {
+//             int levelSize = queue.size();
+//             long sum = 0; 
+            
+//             for (int i = 0; i < levelSize; i++) {
+//                 TreeNode node = queue.poll();
+//                 sum += node.val;
+//                 if (node.left != null) queue.offer(node.left);
+//                 if (node.right != null) queue.offer(node.right);
+//             }
+            
+//             result.add((double) sum / levelSize);
+//         }
+        
+//         return result;
+//     }
+// }
