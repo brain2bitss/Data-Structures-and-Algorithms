0dec12634875dd737db71611b06349a2d1b3ec48
brain2bitss <brain2bitss@gmail.com>
2026-07-02 16:02:18 +0530

    Time: 1 ms (99.88%) | Memory: 82.3 MB (12.35%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -15,15 +15,37 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root == null){
-            return 0;
+        if(root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int level = 1;
+        while(!q.isEmpty()){
+            int levelsize = q.size();
+            for(int i = 0; i < levelsize; i++){
+                TreeNode node = q.poll();
+                if(node.right == null && node.left == null){
+                    return level;
+                }
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            level++;
         }
-        if(root.left == null){
-            return 1 + minDepth(root.right);
-        }
-        if(root.right == null){
-            return 1 + minDepth(root.left);
-        }
-        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        return level;
     }
 }
+
+// class Solution {
+//     public int minDepth(TreeNode root) {
+//         if(root == null){
+//             return 0;
+//         }
+//         if(root.left == null){
+//             return 1 + minDepth(root.right);
+//         }
+//         if(root.right == null){
+//             return 1 + minDepth(root.left);
+//         }
+//         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+//     }
+// }
