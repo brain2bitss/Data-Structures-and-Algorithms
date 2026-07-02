aa2090c7aa8961f516f0bd571abea206ed6b6355
brain2bitss <brain2bitss@gmail.com>
2026-07-02 14:18:42 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (31.21%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -14,21 +14,21 @@
  * }
  */
 class Solution {
-    public boolean fun(TreeNode root, int res, int target){
+    private boolean value = false;
+    public void dfs(TreeNode root, int targetSum, int sum){
         if(root == null){
-            return false;
+            return;
         }
-        
-        res = res + root.val;
-        
-        if(root.left == null && root.right == null){
-            return res == target;
+        sum = sum + root.val;
+        if(root.left == null && root.right == null && sum == targetSum){
+            value = true;
+            return;
         }
-        
-        return fun(root.left, res, target) || fun(root.right, res, target);
+        dfs(root.left, targetSum, sum);
+        dfs(root.right, targetSum,sum);
     }
-    
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return fun(root, 0, targetSum);
+        dfs(root, targetSum , 0);
+        return value;
     }
 }
